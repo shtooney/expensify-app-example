084a08164b1ff05294595a30f629f14714bc8668
Russ <shtooney@yahoo.com>
2019-11-22 01:33:13 -0500

    setup formatting

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -6,5 +6,4 @@ import expenses from '../fixtures/expenses';
 test('should render expense list item', () => {
     const wrapper = shallow(<ExpenseListItem {...expenses[0]}/>);
     expect(wrapper).toMatchSnapshot();
-    //
 });
