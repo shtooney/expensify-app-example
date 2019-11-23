2ef86fb0973ac9eb21d03869842169d1614612c3
Russ <shtooney@yahoo.com>
2019-11-23 15:27:29 -0500

    polyfill etc

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -32,21 +32,30 @@ export class ExpenseListFilters extends React.Component{
 
     render(){
         return(
-            <div>
-                <input 
-                    type="text" 
-                    value={this.props.filters.text} 
-                    onChange={this.onTextChange}
-                >
-                </input>
-                <select 
-                    value={this.props.filters.sortBy} 
-                    onChange={this.onSortChange} 
-                >
-                    <option value="date">Date</option>
-                    <option value="amount">Amount</option>
-                </select>
-                    <DateRangePicker
+            <div className="content-container">
+                <div className="input-group">
+                    <div className="input-group__item">
+                        <input 
+                        type="text"
+                        className="text-input" 
+                        placeholder="Search expenses"
+                        value={this.props.filters.text} 
+                        onChange={this.onTextChange}
+                        >
+                         </input>
+                    </div>
+                    <div className="input-group__item">
+                        <select
+                            className="select" 
+                            value={this.props.filters.sortBy} 
+                            onChange={this.onSortChange} 
+                         >
+                            <option value="date">Date</option>
+                            <option value="amount">Amount</option>
+                        </select>
+                    </div>
+                    <div className="input-group__item">
+                        <DateRangePicker
                         startDate={this.props.filters.startDate} 
                         endDate={this.props.filters.endDate} 
                         onDatesChange={this.onDatesChange}
@@ -55,7 +64,9 @@ export class ExpenseListFilters extends React.Component{
                         showClearDates={true}
                         numberOfMonths={1}
                         isOutsideRange={() => false}
-                    />
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 
 export const Header = ({ startLogout }) => (
-    <header>
-        <h1>Expensify</h1>
-        <div>
-            <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
+    <header className="header">  
+    <div className="content-container">
+        <div className="header__content">
+            <Link className="header__title" to="/dashboard">
+                <h1>Expensify</h1>
+            </Link>            
+            <button className="button button--link" onClick={startLogout}>
+                    Logout
+            </button>
         </div>
-        <div>
-            <NavLink to="/create" activeClassName="is-active" exact={true}>Expense</NavLink>
-        </div>
-        <div>
-            <button onClick={startLogout}>Logout</button>
-        </div>
+    </div>      
     </header>
 );
 
